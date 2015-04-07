2cc09f42cda96fd4268c688e2b8bbca7848be4b0
Nathanael Jones <nathanael.jones@gmail.com>
2015-04-07 19:47:18 -0400

    FastScaling: Align DLL version number with rest of package.

--- Plugins/FastScaling/AssemblyInfo.cpp
+++ Plugins/FastScaling/AssemblyInfo.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) Imazen LLC.
+﻿// Copyright (c) Imazen LLC.
 // No part of this project, including this file, may be copied, modified,
 // propagated, or distributed except as permitted in COPYRIGHT.txt.
 // Licensed under the GNU Affero General Public License, Version 3.0.
@@ -10,22 +10,36 @@ using namespace System::Reflection;
 using namespace System::Runtime::CompilerServices;
 using namespace System::Runtime::InteropServices;
 using namespace System::Security::Permissions;
-
+using namespace ImageResizer::Util;
 //
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //
 [assembly:AssemblyTitleAttribute(L"ImageResizer.Plugins.FastScaling")];
-[assembly:AssemblyDescriptionAttribute(L"")];
-[assembly:AssemblyConfigurationAttribute(L"")];
-[assembly:AssemblyCompanyAttribute(L"")];
-[assembly:AssemblyProductAttribute(L"ImageResizer.Plugins.FastScaling")];
-[assembly:AssemblyCopyrightAttribute(L"Copyright (c)  2014")];
-[assembly:AssemblyTrademarkAttribute(L"")];
-[assembly:AssemblyCultureAttribute(L"")];
 
-//
+// So the ImageResizer knows which bundle this assembly belongs to
+[assembly:EditionAttribute ("R4Performance")];
+
+[assembly:ComVisible(false)];
+
+[assembly:CLSCompliantAttribute(true)];
+
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+#if TRIAL
+[assembly:BuildTypeAttribute ("trial")];
+[assembly:AssemblyProductAttribute ("Image Resizer Plugin - requires license key")];
+#else
+[assembly:AssemblyProductAttribute ("Image Resizer")];
+#endif
+
+[assembly:AssemblyCompanyAttribute ("Imazen LLC")];
+[assembly:AssemblyCopyrightAttribute ("Copyright � 2015 Imazen LLC")];
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -33,11 +47,20 @@ using namespace System::Security::Permissions;
 //      Build Number
 //      Revision
 //
-// You can specify all the value or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:/*
+[assembly:AssemblyVersionAttribute ("4.0.0.0")];
+[assembly:AssemblyFileVersionAttribute ("4.0.0.591")];
+[assembly:AssemblyInformationalVersionAttribute ("4.0.0-prerelease.591")];
 
-[assembly:AssemblyVersionAttribute("1.0.*")];
+[assembly:CommitAttribute ("331d72ae9881981a0cce986083afee8b934f6eae")];
 
-[assembly:ComVisible(false)];
 
-[assembly:CLSCompliantAttribute(true)];
+// These commented out settings are for the build script to access
+// [assembly: PackageName("Resizer")]
+// [assembly: NugetVersionAttribute("4.0.0-prerelease")]
+// [assembly: DownloadServer("http://downloads.imageresizing.net/")]
+[assembly: CommitAttribute("35c8958d6292e5de2a80d7ec553dd86cbb020eae")]
+[assembly: AssemblyVersionAttribute("4.0.0.0")]
+[assembly: AssemblyFileVersionAttribute("4.0.0.515")]
+[assembly: AssemblyInformationalVersionAttribute("4.0.0-prerelease.515")]
